d3b8f0d242f4698ce9a77d01f7c3be7b839dc6e7
Yikai Zhao <yikai@z1k.dev>
2024-03-17 15:08:52 +0800

    fix build-local.sh

--- build-local.sh
+++ build-local.sh
@@ -5,10 +5,10 @@ rm -rf ./dist ./build
 
 mkdir build
 pushd build
-git clone -b json-rpc-29 --depth 1 https://github.com/blahgeek/emacs emacs-src
+git clone -b 29.2 --depth 1 https://github.com/emacs-mirror/emacs emacs-src
 popd
 
 docker build .  # next step has no log, so build first
 IMAGE_ID=$(docker build -q .)
 
-./run-and-package.sh "$IMAGE_ID" ./build/emacs-src BUILD_WITH_X11=no
+./run-and-package.sh "$IMAGE_ID" ./build/emacs-src BUILD_GUI=no
