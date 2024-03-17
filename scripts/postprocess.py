19a2fd97918c9ce49dad25ab23b1ee67ad0c3126
Yikai Zhao <yikai@z1k.dev>
2024-03-17 18:38:53 +0800

    Fix resolving libgccjit dependants

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -20,6 +20,30 @@ def _glob_one(p):
     logging.info(f'Glob {p} -> {res[0]}')
     return Path(res[0])
 
+emacs_version = str(_glob_one(APPDIR / 'bin/emacs-*')).split('-')[-1]
+logging.info(f'Emacs version: {emacs_version}')
+
+all_bins = [f'emacs-{emacs_version}', 'emacs', 'ebrowse', 'emacsclient', 'etags', 'ctags']
+
+# patchelf
+# must do this before processing libraries below, so that we can correctly find dependants of libgccjit.so etc.
+for n in all_bins:
+    subprocess.run(['patchelf', '--set-rpath', '$ORIGIN/../lib', str(APPDIR / 'bin' / n)])
+
+# remove unwanted bins
+for n in (Path(APPDIR) / 'bin').iterdir():
+    if n.name not in all_bins:
+        n.unlink()
+
+# link pdump file
+pdump_file = _glob_one(APPDIR / f'libexec/emacs/{emacs_version}/**/*.pdmp')
+os.symlink('../' +str(pdump_file.relative_to(APPDIR)),
+           str(APPDIR / 'bin' / f'emacs-{emacs_version}.pdmp'))
+
+# copy desktop files
+shutil.copy2(APPDIR / 'share/applications/emacs.desktop', APPDIR / 'emacs.desktop')
+shutil.copy2(APPDIR / 'share/icons/hicolor/128x128/apps/emacs.png', APPDIR / 'emacs.png')
+
 # copy as,ld for libgccjit
 gccjit_libexec_bin_path = _glob_one(APPDIR / 'libexec/gcc/*/*/')
 for n in ['as', 'ld']:
@@ -65,9 +89,11 @@ LIB_WHITELIST = [
 ldd_output = subprocess.check_output(['ldd', str(APPDIR / 'bin/emacs')], universal_newlines=True)
 for line in ldd_output.splitlines():
     if '=>' not in line or 'not found' in line:
+        logging.warning(f'Unexpected ldd output: {line}')
         continue
     libpath = Path(line.split()[2])
     if not libpath.exists():
+        logging.warning(f'Skipping non-exist library {libpath}')
         continue
     if libpath.parent.resolve() == (APPDIR / 'lib'):
         continue
@@ -79,26 +105,3 @@ for line in ldd_output.splitlines():
     dst_path = APPDIR / 'lib' / libpath.name
     shutil.copy2(libpath, dst_path)
     subprocess.run(['patchelf', '--set-rpath', '$ORIGIN', dst_path])
-
-emacs_version = str(_glob_one(APPDIR / 'bin/emacs-*')).split('-')[-1]
-logging.info(f'Emacs version: {emacs_version}')
-
-all_bins = [f'emacs-{emacs_version}', 'emacs', 'ebrowse', 'emacsclient', 'etags', 'ctags']
-
-# patchelf
-for n in all_bins:
-    subprocess.run(['patchelf', '--set-rpath', '$ORIGIN/../lib', str(APPDIR / 'bin' / n)])
-
-# remove unwanted bins
-for n in (Path(APPDIR) / 'bin').iterdir():
-    if n.name not in all_bins:
-        n.unlink()
-
-# link pdump file
-pdump_file = _glob_one(APPDIR / f'libexec/emacs/{emacs_version}/**/*.pdmp')
-os.symlink('../' +str(pdump_file.relative_to(APPDIR)),
-           str(APPDIR / 'bin' / f'emacs-{emacs_version}.pdmp'))
-
-# copy desktop files
-shutil.copy2(APPDIR / 'share/applications/emacs.desktop', APPDIR / 'emacs.desktop')
-shutil.copy2(APPDIR / 'share/icons/hicolor/128x128/apps/emacs.png', APPDIR / 'emacs.png')
