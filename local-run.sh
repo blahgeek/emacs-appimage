df500e765dfe871846b06af22a130f6870233a1c
Yikai Zhao <yikai@z1k.dev>
2024-03-17 18:42:31 +0800

    fix local-run.sh branch

--- local-run.sh
+++ local-run.sh
@@ -5,7 +5,7 @@ rm -rf ./dist ./build
 
 mkdir build
 pushd build
-git clone -b 29.2 --depth 1 https://github.com/emacs-mirror/emacs emacs-src
+git clone -b emacs-29.2 --depth 1 https://github.com/emacs-mirror/emacs emacs-src
 popd
 
 docker build .  # next step has no log, so build first
